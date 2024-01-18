f5e451d52232733bb3840a40879924001d3c3970
abBaptiste <156200375+abBaptiste@users.noreply.github.com>
2024-01-18 14:44:46 +0100

    Add files via upload

---
+++ generateur.php
@@ -0,0 +1,30 @@
+<?php
+include "mesFonctionsGenerales.php";
+
+// Connexion à la base de données BDEtudiant
+$cnxBDD = new mysqli ('localhost', 'root', 'Iroise29' ,'MediTech', 3306);;
+
+// les noms sont dans le fichier nom.txt
+$NomFichier = 'nom.txt';
+$tabloNomFamille = file($NomFichier);
+
+// les prénoms garçon sont dans le fichier garcon.txt
+$NomFichier = 'garcon.txt';
+$tabloPrenom = file($NomFichier);
+
+// rand(x, y) fournit un nombre au hasard entre x et y
+$n = rand(1, sizeof($tabloNomFamille)); // $n contient un rang au hasard
+$p = rand(1, sizeof($tabloPrenom));     // $p contient un rang au hasard
+
+$id = substr($tabloNomFamille[$n], 0, 4);
+//$id= rand (1, $tabloNomFamille[2]);
+// Insertion dans la table ETUDIANT du nième nom de famille et du pième prénom 
+$sql="INSERT INTO visiteur(VIS_ID, VIS_NOM, VIS_PRENOM, VIS_ADRESSE, VIS_CP, VIS_VILLE, VIS_DATE_EMBAUCHE) VALUES ('$id', '$tabloNomFamille', '$tabloPrenom', NULL, NULL, NULL, NULL)";
+
+echo "sql : ".$sql."<br />";
+$result = $cnxBDD->query($sql)
+    or die ("Requête invalide : ".$sql);
+
+// Fermer la connexion MYSQL 
+$cnxBDD->close();
+?>
