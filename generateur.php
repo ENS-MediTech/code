4786333548e1b6a4d567219d0548ae87d3d88c98
abBaptiste <156200375+abBaptiste@users.noreply.github.com>
2024-01-25 15:51:56 +0100

    Fille-garcon-ville-codepostal-generateur.php

--- generateur.php
+++ generateur.php
@@ -1,79 +1,91 @@
 <?php
 include "mesFonctionsGenerales.php";
+
 // Connexion à la base de données BDEtudiant
-$cnxBDD = new mysqli ('localhost', 'root', 'Iroise29' ,'MediTech', 3306);;
-// les noms sont dans le fichier nom.txt
-// debut booucle garcon
-$i = 1;
+$cnxBDD = new mysqli('localhost', 'root', 'Iroise29', 'MediTech', 3306);
+
+// Vérifier la connexion à la base de données
+if ($cnxBDD->connect_error) {
+    die("Erreur de connexion à la base de données : " . $cnxBDD->connect_error);
+}
+
+// Les noms sont dans le fichier nom.txt
 $NomFichier = 'nom.txt';
 $tabloNomFamille = file($NomFichier);
 
-// les prénoms garçon sont dans le fichier garcon.txt
+// Les noms sont dans le fichier prenomfille.txt
+$NomFichier = 'fille.txt';
+$tabloPrenomF = file($NomFichier);
+
+
+// Les prénoms garçon sont dans le fichier garcon.txt
 $NomFichier = 'garcon.txt';
-$tabloPrenom= file($NomFichier);
-    //villes + code postal
+$tabloPrenomG = file($NomFichier);
+
+// Villes + code postal
 $NomFichier = 'codeville.txt';
 $tabloVille = file($NomFichier);
+
+// Boucle garçon
+$i = 1;
 while ($i <= 10) {
+$idGG = "";
+$idG = "";
+    $n = rand(0, sizeof($tabloNomFamille) - 1);
+    $p = rand(0, sizeof($tabloPrenomG) - 1);
+    $v = rand(0, sizeof($tabloVille) - 1);
+    $idGG = substr($tabloNomFamille[$n], 0, 2);
+    $idG = substr($tabloPrenomG[$p], 0, 2);
 
-
-// rand(x, y) fournit un nombre au hasard entre x et y
-$n = rand(1, sizeof($tabloNomFamille)); // $n contient un rang au hasard
-$p = rand(1, sizeof($tabloPrenom));// $p contient un rang au hasard
-$id = substr($tabloNomFamille[$n], 0, 4);
-$v= rand(1, sizeof($tabloVille)); 
-    
-$ligne=$tabloVille[$v];
+    $ligne = $tabloVille[$v];
     $data = explode(';', $ligne);
+
     if (count($data) >= 4) {
-        // Récupère les données nécessaires
         $codeCommune = $data[0];
         $nomCommune = $data[1];
         $codePostal = $data[2];
 
- $sql = "INSERT INTO visiteur(VIS_ID, VIS_NOM, VIS_PRENOM, VIS_ADRESSE, VIS_CP, VIS_VILLE, VIS_DATE_EMBAUCHE) VALUES ('$id', '$tabloNomFamille[$n]', '$tabloPrenom[$p]', NULL, '$codePostal', '$nomCommune', NULL)";
+        $sql = "INSERT INTO visiteur(VIS_ID, VIS_NOM, VIS_PRENOM, VIS_ADRESSE, VIS_CP, VIS_VILLE, VIS_DATE_EMBAUCHE) VALUES ('$idGG$idG', '$tabloNomFamille[$n]', '$tabloPrenomG[$p]', NULL, '$codePostal', '$nomCommune', NULL)";
 
         echo "sql : " . $sql . "<br />";
         $result = $cnxBDD->query($sql) or die("Requête invalide : " . $sql);
     } else {
         echo "Erreur lors de la décomposition des données." . PHP_EOL;
     }
+
+    $i++;
 }
 
-
-
-
-echo "sql : ".$sql."<br />";
-$result = $cnxBDD->query($sql)
-    or die ("Requête invalide : ".$sql);
-$i++;
-}
-//fin garcon
-//boucle fille
+// Boucle fille
 $i = 1;
 while ($i <= 10) {
-// rand(x, y) fournit un nombre au hasard entre x et y
-$n = rand(1, sizeof($tabloNomFamille)); // $n contient un rang au hasard
-$p = rand(1, sizeof($tabloPrenomF));// $p contient un rang au hasard
-$id = substr($tabloNomFamille[$n], 0, 4);
-    $v= rand(1, sizeof($tabloVille)); 
-    
-$ligne=$tabloVille[$v];
+    $idFF = "";
+    $idF = "";
+    $n = rand(0, sizeof($tabloNomFamille) - 1);
+    $p = rand(0, sizeof($tabloPrenomF) - 1);
+    $v = rand(0, sizeof($tabloVille) - 1);
+    $idFF = substr($tabloNomFamille[$n], 0, 2);
+    $idF = substr($tabloPrenomF[$p], 0, 2);
+
+    $ligne = $tabloVille[$v];
     $data = explode(';', $ligne);
+
     if (count($data) >= 4) {
-        // Récupère les données nécessaires
         $codeCommune = $data[0];
         $nomCommune = $data[1];
         $codePostal = $data[2];
-//$id= rand (1, $tabloNomFamille[2]); mini code de Imaaaane
-// Insertion dans la table ETUDIANT du nième nom de famille et du pième prénom 
-$sql="INSERT INTO visiteur(VIS_ID, VIS_NOM, VIS_PRENOM, VIS_ADRESSE, VIS_CP, VIS_VILLE, VIS_DATE_EMBAUCHE) VALUES ('$id', '$tabloNomFamille[$n]', '$tabloPrenomF[$p]', NULL,'$codePostal', '$nomCommune', NULL)";
 
-echo "sql : ".$sql."<br />";
-$result = $cnxBDD->query($sql)
-    or die ("Requête invalide : ".$sql);
-$i++;
+        $sql = "INSERT INTO visiteur(VIS_ID, VIS_NOM, VIS_PRENOM, VIS_ADRESSE, VIS_CP, VIS_VILLE, VIS_DATE_EMBAUCHE) VALUES ('$id$idF', '$tabloNomFamille[$n]', '$tabloPrenomF[$p]', NULL, '$codePostal', '$nomCommune', NULL)";
+
+        echo "sql : " . $sql . "<br />";
+        $result = $cnxBDD->query($sql) or die("Requête invalide : " . $sql);
+    } else {
+        echo "Erreur lors de la décomposition des données." . PHP_EOL;
+    }
+
+    $i++;
 }
-// Fermer la connexion MYSQL 
+
+// Fermer la connexion MySQL
 $cnxBDD->close();
 ?>
