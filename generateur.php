9bc66d5eed4b8be9ee81a93cf254cab503e01425
abBaptiste <156200375+abBaptiste@users.noreply.github.com>
2024-01-25 14:49:06 +0100

    fille-garçon- generateur.php

--- generateur.php
+++ generateur.php
@@ -6,7 +6,7 @@ $cnxBDD = new mysqli ('localhost', 'root', 'Iroise29' ,'MediTech', 3306);;
 
 
 // les noms sont dans le fichier nom.txt
-// debut
+// debut booucle garcon
 $i = 1;
 while ($i <= 10) {
 $NomFichier = 'nom.txt';
@@ -14,7 +14,7 @@ $tabloNomFamille = file($NomFichier);
 
 // les prénoms garçon sont dans le fichier garcon.txt
 $NomFichier = 'garcon.txt';
-$tabloPrenom = file($NomFichier);
+$tabloPrenom G= file($NomFichier);
 
 
 $NomFichier = 'codeville.txt';
@@ -22,18 +22,45 @@ $tabloVille = file($NomFichier);
 
 // rand(x, y) fournit un nombre au hasard entre x et y
 $n = rand(1, sizeof($tabloNomFamille)); // $n contient un rang au hasard
-$p = rand(1, sizeof($tabloPrenom));// $p contient un rang au hasard
+$p = rand(1, sizeof($tabloPrenomG));// $p contient un rang au hasard
 $id = substr($tabloNomFamille[$n], 0, 4);
 //$id= rand (1, $tabloNomFamille[2]); mini code de Imaaaane
 // Insertion dans la table ETUDIANT du nième nom de famille et du pième prénom 
-$sql="INSERT INTO visiteur(VIS_ID, VIS_NOM, VIS_PRENOM, VIS_ADRESSE, VIS_CP, VIS_VILLE, VIS_DATE_EMBAUCHE) VALUES ('$id', '$tabloNomFamille[$n]', '$tabloPrenom[$p]', NULL, '$commune', '$cp', NULL)";
+$sql="INSERT INTO visiteur(VIS_ID, VIS_NOM, VIS_PRENOM, VIS_ADRESSE, VIS_CP, VIS_VILLE, VIS_DATE_EMBAUCHE) VALUES ('$id', '$tabloNomFamille[$n]', '$tabloPrenomG[$p]', NULL, '$commune', '$cp', NULL)";
 
 echo "sql : ".$sql."<br />";
 $result = $cnxBDD->query($sql)
     or die ("Requête invalide : ".$sql);
 $i++;
 }
-//fin
+//fin garcon
+//boucle fille
+$i = 1;
+while ($i <= 10) {
+$NomFichier = 'nom.txt';
+$tabloNomFamille = file($NomFichier);
+
+// les prénoms garçon sont dans le fichier garcon.txt
+$NomFichier = 'fille.txt';
+$tabloPrenomF = file($NomFichier);
+
+
+$NomFichier = 'codeville.txt';
+$tabloVille = file($NomFichier);
+
+// rand(x, y) fournit un nombre au hasard entre x et y
+$n = rand(1, sizeof($tabloNomFamille)); // $n contient un rang au hasard
+$p = rand(1, sizeof($tabloPrenomF));// $p contient un rang au hasard
+$id = substr($tabloNomFamille[$n], 0, 4);
+//$id= rand (1, $tabloNomFamille[2]); mini code de Imaaaane
+// Insertion dans la table ETUDIANT du nième nom de famille et du pième prénom 
+$sql="INSERT INTO visiteur(VIS_ID, VIS_NOM, VIS_PRENOM, VIS_ADRESSE, VIS_CP, VIS_VILLE, VIS_DATE_EMBAUCHE) VALUES ('$id', '$tabloNomFamille[$n]', '$tabloPrenomF[$p]', NULL, '$commune', '$cp', NULL)";
+
+echo "sql : ".$sql."<br />";
+$result = $cnxBDD->query($sql)
+    or die ("Requête invalide : ".$sql);
+$i++;
+}
 // Fermer la connexion MYSQL 
 $cnxBDD->close();
 ?>
