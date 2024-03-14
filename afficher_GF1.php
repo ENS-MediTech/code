fdf98ee7d643e6c0b4b2e7fb3a6d745623ffb055
alejeune1 <156212652+alejeune1@users.noreply.github.com>
2024-03-14 17:11:57 +0100

    afficher_GF1.php

--- afficher_GF1.php
+++ afficher_GF1.php
@@ -8,7 +8,7 @@ if ($cnxBDD->connect_error) {
 $nom = $cnxBDD->real_escape_string($nom);
 $prenom = $cnxBDD->real_escape_string($prenom);
 
-$requete = "SELECT nom, prenom, date FROM visiteur";
+$requete = "SELECT VIS_NOM, VIS_PRENOM, VIS_DATE_EMBAUCHE FROM visiteur";
 
 $resultat = $cnxBDD->query($requete);
 
@@ -17,9 +17,9 @@ if (!$resultat) {
 }
 
 while ($ligne = $resultat->fetch_assoc()) {
-    echo "Nom : " . $ligne['nom'] . "<br>";
-    echo "Prénom : " . $ligne['prenom'] . "<br>";
-    echo "Date : " . $ligne['date'] . "<br>";
+    echo "Nom : " . $ligne['VIS_NOM'] . "<br>";
+    echo "Prénom : " . $ligne['VIS_PRENOM'] . "<br>";
+    echo "Date : " . $ligne['VIS_DATE_EMBAUCHE'] . "<br>";
     echo "<br>";
 }
 
