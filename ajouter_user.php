99d7c4c40633c9b39b5923d19f1e673de646a746
ImaneWmr <156522795+ImaneWmr@users.noreply.github.com>
2024-04-04 13:57:01 +0200

    ajouter_user.php

--- ajouter_user.php
+++ ajouter_user.php
@@ -3,18 +3,18 @@ $serveur = "localhost";
 $utilisateur = "root";
 $mot_de_passe = "Iroise29";
 $nom_base_de_donnees = "MediTech";
-$nom_table = "visiteur";
 $numero_port = 3306;
 
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
-    $nom = $_GET["VIS_NOM"];
-    $prenom = $_GET["VIS_PRENOM"];
-    $adresse = $_GET["VIS_ADRESSE"];
-    $ville = $_GET["VIS_VILLE"];
-    $cp = $_GET["VIS_CP"];
-    $date_embauche = $_GET["VIS_DATE_EMBAUCHE"];
-   // $login = $_GET["login"];
-   // $mdp = $_GET["mdp"];
+    $identifiant = $_GET["identifi"];
+    $nom = $_GET["nom"];
+    $prenom = $_GET["prenom"];
+    $adresse = $_GET["adresse"];
+    $ville = $_GET["ville"];
+    $cp = $_GET["cp"];
+    $date_embauche = $_GET["date1"];
+    $login = $_GET["login"];
+    $mdp = $_GET["mdp"];
 
     $connexion = mysqli_connect($serveur, $utilisateur, $mot_de_passe, $nom_base_de_donnees);
 
@@ -22,14 +22,23 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
         die("Échec de la connexion : " . mysqli_connect_error());
     }
 
-    $requete = "INSERT INTO $nom_table (VIS_NOM, VIS_PRENOM, VIS_ADRESSE, VIS_VILLE, VIS_CP, VIS_DATE_EMBAUCHE) VALUES ('$nom', '$prenom', '$adresse', '$ville', '$cp', '$date_embauche')";
-
+    // Requête d'insertion dans la table 'visiteur'
+    $requete = "INSERT INTO visiteur (VIS_ID, VIS_NOM, VIS_PRENOM, VIS_ADRESSE, VIS_CP, VIS_VILLE, VIS_DATE_EMBAUCHE) VALUES ('$identifiant', '$nom', '$prenom', '$adresse', '$cp', '$ville', '$date_embauche')";
     if (mysqli_query($connexion, $requete)) {
-        echo "Utilisateur ajouté avec succès.";
+        // Requête d'insertion dans la table 'USER'
+        $requete2= "INSERT INTO USER (id, login, password, dteConnexion, role) VALUES ('$identifiant', '$login' , '$mdp', NULL, NULL)";
+        if (mysqli_query($connexion, $requete2)) {
+            echo "Utilisateur ajouté avec succès.";
+        } 
+            else {
+                echo "Erreur lors de l'ajout de l'utilisateur : " . mysqli_error($connexion);
+                echo "Requête SQL : " . $requete2; // Affichez la requête pour débogage
+            }
+        
     } else {
-        echo "Erreur lors de l'ajout de l'utilisateur : " . mysqli_error($connexion);
+        echo "Erreur lors de l'ajout du visiteur : " . mysqli_error($connexion);
     }
 
+    // Fermeture de la connexion
     mysqli_close($connexion);
 }
-?>
